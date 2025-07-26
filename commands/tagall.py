10b29941534b2e1a2e8d2e3f63e5966d3958fc10
ᏦᎽᎾᎿᎯᏦᎯ <stromjersey@gmail.com>
2025-07-26 02:17:36 +0200

    Update tagall.py

--- commands/tagall.py
+++ commands/tagall.py
@@ -4,13 +4,13 @@ from pyrogram.errors import FloodWait
 import asyncio
 
 @Client.on_message(filters.command("tagall") & filters.group)
-async def tag_all(_, message: Message):
+async def tagall(_, message: Message):
     if not message.from_user or not message.from_user.id:
         return
     user = await message.chat.get_member(message.from_user.id)
     if not user.status in ("administrator", "creator"):
         return await message.reply("Seuls les admins peuvent utiliser cette commande.")
-    
+
     limit = 30 
     mentions = []
     async for member in message.chat.iter_members():
