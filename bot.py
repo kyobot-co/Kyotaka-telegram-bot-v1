7bf921613f203c5e17a50d68144e86deffa78c8d
kyobot-co <botwhatsapp248@gmail.com>
2025-08-23 18:17:32 +0100

    Update bot.py

--- bot.py
+++ bot.py
@@ -7,7 +7,7 @@ from telegram.ext import ApplicationBuilder, CommandHandler, ContextTypes, Messa
 import time
 import os
 
-TOKEN = os.getenv("TELEGRAM_TOKEN", "LE_TOKEN_DE_TON_BOT_ICI")
+TOKEN = os.getenv("TELEGRAM_TOKEN", "8423814598:AAGuP9LZH2zgKAdxNyBWx3viNzq302bHyVY")
 PORT = int(os.environ.get("PORT", 10000))
 
 logging.basicConfig(
